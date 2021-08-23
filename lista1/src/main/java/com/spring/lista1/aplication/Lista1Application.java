ade76178eedad6fdf29420422f48c6c1105099e1
Cássia <cassianicolau.ca@gmail.com>
2021-08-23 14:45:37 -0300

    LISTA1

---
+++ lista1/src/main/java/com/spring/lista1/aplication/Lista1Application.java
@@ -0,0 +1,13 @@
+package com.spring.lista1.aplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lista1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lista1Application.class, args);
+	}
+
+}
---
+++ lista1/src/test/java/com/spring/lista1/Lista1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.lista1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lista1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
